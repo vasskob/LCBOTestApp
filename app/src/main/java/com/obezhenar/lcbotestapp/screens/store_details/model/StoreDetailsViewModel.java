aae62d0aa0a5a39dec5964bcea47869f0a4e6ff9
Oleg Bezhenar <obezhenar@sharpminds.com>
2017-02-17 17:24:41 +0200

    add : Store details layout

---
+++ app/src/main/java/com/obezhenar/lcbotestapp/screens/store_details/model/StoreDetailsViewModel.java
@@ -0,0 +1,10 @@
+package com.obezhenar.lcbotestapp.screens.store_details.model;
+
+import java.util.List;
+
+public class StoreDetailsViewModel {
+    private double latitude;
+    private double longitude;
+    private List<String> features;
+    private String phone;
+}
